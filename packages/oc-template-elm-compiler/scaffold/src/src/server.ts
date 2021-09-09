6e1224f9061bcd8c2622ab9e34d8afdcbf9c864f
Ricardo Devis Agullo <mkredz@gmail.com>
2021-09-09 15:58:06 +0200

    Update scaffold

--- packages/oc-template-elm-compiler/scaffold/src/src/server.ts
+++ packages/oc-template-elm-compiler/scaffold/src/src/server.ts
@@ -1,33 +1,19 @@
-import { AdditionalData, ClientProps, OcParameters } from './types';
-
 const database = [
-  { name: 'John Doe', age: 34, hobbies: ['Swimming', 'Basketball'] },
-  { name: 'Jane Doe', age: 35, hobbies: ['Running', 'Rugby'] }
+  { name: 'John Doe', email: 'john@doe.com' },
+  { name: 'Jane Doe', email: 'jane@doe.com' }
 ];
 
 async function getUser(userId: number) {
-  return database[userId];
+  return database[userId] || database[0];
 }
 
-export async function data(
-  context: any,
-  callback: (error: any, data: ClientProps | AdditionalData) => void
-) {
+export async function data(context: any, callback: (error: any, data: any) => void) {
   const { userId } = context.params;
-  const user = await getUser(userId);
-  const shouldGetMoreData = context.params.getMoreData;
-  const [firstName, lastName] = user.name.split(/\s+/);
-
-  if (shouldGetMoreData) {
-    return callback(null, {
-      age: user.age,
-      hobbies: user.hobbies
-    });
-  }
+  const { email, name } = await getUser(userId);
 
   return callback(null, {
     userId,
-    firstName,
-    lastName
+    name,
+    email
   });
 }
